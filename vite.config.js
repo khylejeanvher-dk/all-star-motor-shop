16e2d0245d68f0c312a212eb8cd49ef5a5db65c0
khylejeanvher-dk <khylejeanvher@gmail.com>
2026-03-12 01:07:31 +0800

    fix: multi-page vite build + vercel routing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   server: {
     port: 3000
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main:     resolve(__dirname, 'index.html'),
+        product:  resolve(__dirname, 'product.html'),
+        category: resolve(__dirname, 'category.html'),
+        about:    resolve(__dirname, 'about.html'),
+        contact:  resolve(__dirname, 'contact.html'),
+      }
+    }
   }
 })
